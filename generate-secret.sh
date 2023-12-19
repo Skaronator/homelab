2f3d31ac87b05ecb1be494e93af27c20ba1a2dd1
Niklas Wagner <Skaro@Skaronator.com>
2023-12-19 15:46:33 +0100

    Allow passing passwordfile to generate-secret

--- generate-secret.sh
+++ generate-secret.sh
@@ -1,31 +1,33 @@
 #!/usr/bin/env bash
 
-password=$1
-
-if [ -z "$password" ]
-then
-echo ""
-echo "No password set. Picking one randomly.."
-password=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 64)
-echo ""
-echo "New password: $password"
-echo ""
-else
-echo ""
-echo "Using provided password: $password"
-echo ""
+if [ "$#" -eq 0 ]; then
+  echo "Usage: $0 [<password_file> | <password>]"
+  exit 1
 fi
+
+if [ -f "$1" ]; then
+  password=$(cat "$1")
+  echo "Using password from file: $1"
+else
+  password=$1
+  if [ -z "$password" ]; then
+    echo "No password set. Generating a random password..."
+    password=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 64)
+  else
+    echo "Using provided password."
+  fi
+fi
+
+echo "Password: $password"
 
 export SEALED_SECRETS_CONTROLLER_NAMESPACE="ops"
 export SEALED_SECRETS_CONTROLLER_NAME="sealed-secrets"
 export SEALED_SECRETS_SCOPE="cluster-wide"
 
-
 encryptedPassword=$(kubectl create secret generic script --dry-run=client --from-literal "script=$password" -o json | kubeseal -o json | jq -r '.spec.encryptedData.script')
 
 echo ""
-echo "Encypted password:"
-echo ""
+echo "Encrypted password:"
 echo "$encryptedPassword"
 echo ""
 
